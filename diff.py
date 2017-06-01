792e4c7a5e98844494d481f1de295d5838b4d3e4
MrGold <dgold@sjnma.org>
2017-06-01 14:54:16 -0500

    git branching somewhat working- committing to work on solution from home

--- diff.py
+++ diff.py
@@ -1,11 +1,15 @@
 from url import urls
 import git, os, shutil
+import subprocess
+
 
 class diff():
     def __init__(self):
         self.DIR_NAME = "student_work"
         self.master = os.path.join(self.DIR_NAME,"master")
         self.project_name="DiceDuels"
+        self.projects={}
+        self.students={}
         try:
             os.stat(self.DIR_NAME)
         except:
@@ -36,29 +40,70 @@ class diff():
         origin.fetch()
         origin.pull(origin.refs[0].remote_head)
 
-        print "---- DONE ----"
+        self.students[student]=project_folder
+
         return project_folder
 
     def GetAllEntries(self,repo):#todo change loop to remove redundancies
         for student1,url1 in urls.iteritems():
-            for student2,url2 in urls.iteritems():
-                print("downlaoded ->",self.GetCodeFromGithub(url1,student1))
-                print("downlaoded ->",self.GetCodeFromGithub(url2,student2))
+            print("downlaoded ->",self.GetCodeFromGithub(url1,student1))
 
-    def CompareTwoEntries(self,entryPath1,entryPath2):
-        print entryPath1
-        print("------")
-        print entryPath2
+    def CompareTwoEntries(self,branchname1,branchname2):
+        print("comparing"+branchname1+"<>"+branchname2)
+        with open('out-file.txt', 'w') as f:
+            update_branches=subprocess.Popen(["git", "diff", "remotes/"+branchname1+"/master","remotes/"+branchname2+"/master"], stdout=f)
+            update_branches.wait()
+
+        #git diff master remotes/b/master
+
+
+
     def CompareAllEntries(self):
         if os.path.isdir(self.master):
             shutil.rmtree(self.master)
 
-        os.mkdir(master)
+        os.mkdir(self.master)
         repo = git.Repo.init(self.master)
 
+        #add branches to master repo
+        for student,project_folder in self.students.iteritems():
+            print "adding",project_folder
+            branch_add_output=subprocess.Popen(["git", "remote", "add", "-f",student,os.path.join(".",project_folder,".git")], stdout=subprocess.PIPE)
+            branch_add_output.wait()
+            print branch_add_output
+
+        print "fetching latest code for all students from cloned repositories"
+        update_branches=subprocess.Popen(["git", "remote", "update"])
+        update_branches.wait()
+
+
+        '''
+        #diff all students
+        for student1,project_folder1 in self.students.iteritems():
+            for student2,project_folder2 in self.students.iteritems():
+                self.CompareTwoEntries(student1,student2)
+                '''
+
+
+        #remove branches from master repo
+        for student,project_folder in self.students.iteritems():
+            print "removing",student
+            branch_remove_output=subprocess.Popen(["git", "remote", "rm", student], stdout=subprocess.PIPE)
+            branch_remove_output.wait()
+            print branch_remove_output
+        print self.students
+
+
+        '''
+git remote add -f b path/to/repo_b.git
+git remote update
+git diff master remotes/b/master
+git remote rm b
+        '''
 
 
 
 d1 = diff()
 
 d1.GetAllEntries("DiceDuels")
+d1.CompareAllEntries()
